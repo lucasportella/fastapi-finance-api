4733cdb5509da8566e8a234686a1ce40c205a850
lucas <lucasportellaagu@gmail.com>
2025-09-09 20:49:37 -0300

    feat: add user_repository

---
+++ core/repositories/user_repository.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from core.entities.user import User
+from typing import Optional, List
+
+class UserRepository(ABC):
+  
+  @abstractmethod
+  def add(self, user: User) -> User:
+    pass
+  
+  @abstractmethod
+  def get_by_id(self, user_id: int) -> Optional[User]:
+    pass
+  
+  @abstractmethod
+  def get_all(self) -> List[User]:
+    pass
+  
+  @abstractmethod
+  def update(self, user: User) -> Optional[User]:
+    pass
+  
+  @abstractmethod
+  def delete(self, user_id: int) -> bool:
+    pass
