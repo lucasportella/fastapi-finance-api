c452a85c728d2ade0b1efeab660748b3a0ea9704
lucas <lucasportellaagu@gmail.com>
2025-09-11 20:02:19 -0300

    feat: Loan and Property repositories

---
+++ core/repositories/loan_repository.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from core.entities.loan import Loan
+
+class LoanRepository(ABC):
+  @abstractmethod
+  def add(self, loan: Loan) -> Loan:
+    pass
+  
+  @abstractmethod
+  def get_all(self) -> List[Loan]:
+    pass
+  
+  @abstractmethod
+  def get_by_id(self, loan_id: int) -> Optional[Loan]:
+    pass
+  
+  @abstractmethod
+  def update(self, loan: Loan) -> Optional[Loan]:
+    pass
+  
+  @abstractmethod
+  def delete(self, loan_id: int) -> bool:
+    pass
---
+++ core/repositories/property_repository.py
@@ -0,0 +1,24 @@
+from abc import ABC,abstractmethod
+from typing import List, Optional
+from core.entities.property import property
+
+class PropertyRepository(ABC):
+  @abstractmethod
+  def add(self, property: property) -> property:
+    pass
+
+  @abstractmethod
+  def get_by_id(self, property_id: int) -> property:
+    pass
+  
+  @abstractmethod
+  def get_all(self) -> List[property]:
+    pass
+  
+  @abstractmethod
+  def update(self, property: property) -> Optional[property]:
+    pass
+
+  @abstractmethod
+  def delete(self, property_id: int) -> bool:
+    pass
