00727b6b36cfb195171baebb678d34a4a79a06ae
lucas <lucasportellaagu@gmail.com>
2025-09-16 16:56:54 -0300

    refactor: rename folder structure

--- core/domain/repositories/property_repository.py
+++ core/domain/repositories/property_repository.py
@@ -1,6 +1,6 @@
 from abc import ABC,abstractmethod
 from typing import List, Optional
-from core.entities.property import property
+from core.domain.entities.property import property
 
 class PropertyRepository(ABC):
   @abstractmethod
--- core/domain/repositories/user_repository.py
+++ core/domain/repositories/user_repository.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from core.entities.user import User
+from core.domain.entities.user import User
 from typing import Optional, List
 
 class UserRepository(ABC):
