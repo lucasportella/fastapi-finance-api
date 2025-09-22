15f2919781ceea301534f1135e064d168724d39c
lucas <lucasportellaagu@gmail.com>
2025-09-22 19:13:42 -0300

    style: remove wrong import

--- app/presentation/routes/user_routes.py
+++ app/presentation/routes/user_routes.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 from app.domain.entities.user import User
 from app.use_cases.create_user import CreateUser
-from app.infrastructure.database.mongo_user_repository import MongoUserRepository
 from app.presentation.routes.dependencies.user_dependencies import get_create_user_case
 
 router = APIRouter()
