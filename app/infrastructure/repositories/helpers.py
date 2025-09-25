b2555557ac24eeb6b4fddc1293a7bc8f92bcb3c9
lucas <lucasportellaagu@gmail.com>
2025-09-25 15:31:15 -0300

    fix(helpers): fix return type

--- app/infrastructure/repositories/helpers.py
+++ app/infrastructure/repositories/helpers.py
@@ -1,7 +1,10 @@
-def doc_to_user(doc):
+from app.domain.entities.user import User
+from typing import List
+
+def doc_to_user(doc) -> User:
   doc["id"] = str(doc["_id"])
   doc.pop("_id")
-  return doc
+  return User(**doc)
 
-def docs_to_users(docs):
+def docs_to_users(docs) -> List[User]:
   return [doc_to_user(doc) for doc in docs]
